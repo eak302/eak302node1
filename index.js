6f7375cc72796e811e4886329e3f8482679b6735
eak302 <eak302@gmail.com>
2016-11-25 18:48:35 +0700

    this edit index.js and add test.php

--- index.js
+++ index.js
@@ -23,11 +23,14 @@ app.get('/', function(request, response) {
 });
 
 app.get('/webhook', function(req, res) {
-    if (req.query['hub.verify_token'] === 'verify_token') {
-      res.send(req.query['hub.challenge']);
-    } else {
-      res.send('Error, wrong validation token');    
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        }
     }
+    res.sendStatus(200);
     //response.send('this is testbot server');
     /*var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
@@ -44,4 +47,22 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
 
