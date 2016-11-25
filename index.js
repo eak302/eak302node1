69042eaad2ca83cc1ede89d17b090813ee1e02cd
eak302 <eak302@gmail.com>
2016-11-25 17:23:00 +0700

    this edit

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/webhook', function(request, response) {
+    response.send('this is testbot server');
+   //response.send(cool()); 
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
